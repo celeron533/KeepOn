377241b2663147f7f5128fb72d4fbe42bd47fb62
celeron533 <zhuyilun8@hotmail.com>
2017-04-27 20:49:42 +0800

    Refine code and add Enable check item in the tray menu

--- KeepOn/Native.cs
+++ KeepOn/Native.cs
@@ -13,26 +13,36 @@ namespace KeepOn
         [Flags]
         public enum EXECUTION_STATE : uint
         {
-            // Enables away mode. This value must be specified with ES_CONTINUOUS.
-            // Away mode should be used only by media-recording and media-distribution applications
-            // that must perform critical background processing on desktop computers
-            // while the computer appears to be sleeping. See Remarks.
+            /// <summary>
+            /// Enables away mode. This value must be specified with ES_CONTINUOUS.
+            /// Away mode should be used only by media-recording and media-distribution applications
+            /// that must perform critical background processing on desktop computers
+            /// while the computer appears to be sleeping. See Remarks.
+            /// </summary>
             ES_AWAYMODE_REQUIRED = 0x00000040,
 
-            // Informs the system that the state being set should remain in effect
-            // until the next call that uses ES_CONTINUOUS and one of the other state flags is cleared.
+            /// <summary>
+            /// Informs the system that the state being set should remain in effect
+            /// until the next call that uses ES_CONTINUOUS and one of the other state flags is cleared.
+            /// </summary>
             ES_CONTINUOUS = 0x80000000,
 
-            // Forces the display to be on by resetting the display idle timer.
+            /// <summary>
+            /// Forces the display to be on by resetting the display idle timer.
+            /// </summary>
             ES_DISPLAY_REQUIRED = 0x00000002,
 
-            // Forces the system to be in the working state by resetting the system idle timer.
-            ES_SYSTEM_REQUIRED = 0x00000001
+            /// <summary>
+            /// Forces the system to be in the working state by resetting the system idle timer.
+            /// </summary>
+            ES_SYSTEM_REQUIRED = 0x00000001,
 
             // Legacy flag, should not be used.
-            // This value is not supported. If ES_USER_PRESENT is combined with other esFlags values,
-            // the call will fail and none of the specified states will be set.
-            //// ES_USER_PRESENT = 0x00000004
+            /// <summary>
+            /// This value is not supported. If ES_USER_PRESENT is combined with other esFlags values,
+            /// the call will fail and none of the specified states will be set.
+            /// </summary>
+            ES_USER_PRESENT = 0x00000004
         }
     }
 }
--- KeepOn/TrayApplicationContext.cs
+++ KeepOn/TrayApplicationContext.cs
@@ -12,29 +12,31 @@ namespace KeepOn
     {
         private NotifyIcon trayIcon;
         private AboutBox aboutBox = new AboutBox();
+        private Config config = new Config();
+        private ContextMenu trayMenu;
+
+        private MenuItem EnableItem;
+        private MenuItem AboutItem;
+        private MenuItem ExitItem;
 
         public TrayApplicationContext()
         {
-            // Prevent system auto lock or shut down the monitor
-            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS |
-                                    EXECUTION_STATE.ES_DISPLAY_REQUIRED |
-                                    EXECUTION_STATE.ES_SYSTEM_REQUIRED);
+            SetState(config);
+            InitMenu();
 
             // Resume everything when exit the application
             Application.ApplicationExit += (sender, e) =>
             {
-                SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+                UnsetState();
             };
+
 
             // Initialize tray icon
             trayIcon = new NotifyIcon()
             {
                 Icon = Resources.AppIcon,
                 Text = "'KeepOn' is running",
-                ContextMenu = new ContextMenu(new MenuItem[] {
-                    new MenuItem("About", About),
-                    new MenuItem("Exit", Exit)
-            }),
+                ContextMenu = trayMenu,
                 Visible = true
             };
 
@@ -42,8 +44,35 @@ namespace KeepOn
             trayIcon.ShowBalloonTip(3000);
         }
 
+        void InitMenu()
+        {
+            trayMenu = new ContextMenu(new MenuItem[] {
+                         EnableItem = new MenuItem("Enable", Enable_Clicked),
+                         AboutItem = new MenuItem("About", About_Clicked),
+                         ExitItem = new MenuItem("Exit", Exit_Clicked)
+            });
+            RefreshMenu();
+        }
 
-        void Exit(object sender, EventArgs e)
+        void RefreshMenu()
+        {
+            EnableItem.Checked = config.isEnable;
+        }
+
+        void Enable_Clicked(object sender, EventArgs e)
+        {
+            config.isEnable = !config.isEnable;
+            SetState(config);
+            RefreshMenu();
+        }
+
+        void About_Clicked(object sender, EventArgs e)
+        {
+            if (!aboutBox.Visible)
+                aboutBox.ShowDialog();
+        }
+
+        void Exit_Clicked(object sender, EventArgs e)
         {
             // Hide tray icon, otherwise it will remain shown until user move mouse over it
             trayIcon.Visible = false;
@@ -51,10 +80,22 @@ namespace KeepOn
         }
 
 
-        void About(object sender, EventArgs e)
+        EXECUTION_STATE SetState(Config config)
         {
-            if (!aboutBox.Visible)
-                aboutBox.ShowDialog();
+            if (config.isEnable)
+            {
+                return SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS |
+                                            EXECUTION_STATE.ES_DISPLAY_REQUIRED |
+                                            EXECUTION_STATE.ES_SYSTEM_REQUIRED);
+            }
+            else
+                return UnsetState();
+
+        }
+
+        EXECUTION_STATE UnsetState()
+        {
+            return SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
         }
     }
 }
