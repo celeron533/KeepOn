9355a29be44c3b76305c189c8b055839af55dd53
celeron533 <zhuyilun8@hotmail.com>
2022-01-19 10:17:39 +0800

    Bump version 0.1.6

--- KeepOn/Properties/AssemblyInfo.cs
+++ KeepOn/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("https://github.com/celeron533")]
 [assembly: AssemblyProduct("KeepOn")]
-[assembly: AssemblyCopyright("Copyright ©  2017 celeron533")]
+[assembly: AssemblyCopyright("Copyright ©  2022 celeron533")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.5.0")]
-[assembly: AssemblyFileVersion("0.1.5.0")]
+[assembly: AssemblyVersion("0.1.6.0")]
+[assembly: AssemblyFileVersion("0.1.6.0")]
